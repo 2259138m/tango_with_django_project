cd79e58c6deab5220f0d508a5bef97b4418ee66a
Lewis McGlinchey <2259138M@student.gla.ac.uk>
2021-01-20 21:16:49 +0000

    second commit

---
+++ rango/urls.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 20 21:09:15 2021
+
+@author: lewis
+"""
+
+from django.urls import path
+from rango import views
+
+app_name = 'rango'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
