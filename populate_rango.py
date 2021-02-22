bafb1142423c615417422be4b6451d0b958e0716
Lewis McGlinchey <2259138M@student.gla.ac.uk>
2021-02-22 14:23:38 +0000

    C5 test

--- populate_rango.py
+++ populate_rango.py
@@ -34,8 +34,8 @@ def populate():
         {'title':'Flask',
          'url':'http://flask.pocoo.org'} ]
 
-    cats = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes':64},
+            'Django': {'pages': django_pages, 'views': 64, 'likes':32},
             'Other Frameworks': {'pages': other_pages} }
 
 # If you want to add more categories or pages,
@@ -43,10 +43,9 @@ def populate():
 # The code below goes through the cats dictionary, then adds each category,
 # and then adds all the associated pages for that category.
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
-
 # Print out the categories we have added.
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
